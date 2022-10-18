52b3e6d4cb713502b04106381f4e08996b49759c
Felix Rabinovich <felixr@atims.com>
2022-10-18 14:01:55 -0700

    Countries Controller

---
+++ LoaData/Dtos/CountryPopulation.cs
@@ -0,0 +1,8 @@
+﻿namespace WorldCitiesApi.Dtos
+{
+    public class CountryPopulation
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+    }
+}
--- WorldCitiesModel/Models/WorldCitiesContext.cs
+++ WorldCitiesModel/Models/WorldCitiesContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace WorldCitiesModel.Models
 {
@@ -23,8 +24,12 @@ namespace WorldCitiesModel.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=WorldCitiesGolden;Trusted_Connection=True");
+                IConfigurationBuilder builder = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", optional: false)
+                    .AddJsonFile("appsettings.development.json", optional: true);
+                IConfigurationRoot configuration = builder.Build();
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
             }
         }
 
