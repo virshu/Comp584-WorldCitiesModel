d354bfa0d464951a4c1e56362aae59108421fc66
virshu <felix@rabinovich.org>
2022-11-20 13:52:05 -0800

    Added Authentication

---
+++ LoaData/Dtos/LoginRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorldCitiesApi.Dtos;
+
+public class LoginRequest {
+    [Required(ErrorMessage = "User Name is required.")]
+    public string UserName { get; set; } = null!;
+    [Required(ErrorMessage = "Password is required.")]
+    public string Password { get; set; } = null!;
+
+}
--- LoaData/Program.cs
+++ LoaData/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using WorldCitiesApi;
 using WorldCitiesModel.Models;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -13,14 +18,64 @@ builder.Services.AddCors(options => options.AddPolicy("AllowAnything", policy =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Contact = new OpenApiContact {
+            Email = "frabinovich@csun.edu",
+            Name = "Felix Rabinovich",
+            Url = new Uri("https://canvas.csun.edu/courses/131512")
+        },
+        Description = "APIs for World Cities",
+        Title = "World Cities APIs",
+        Version = "V1"
+    });
+    OpenApiSecurityScheme jwtSecurityScheme = new() {
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Name = "JWT Authentication",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Description = "Please enter *only* JWT token",
+        Reference = new OpenApiReference {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, jwtSecurityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            jwtSecurityScheme, Array.Empty<string>()
+        }
+    });
+});
 
 // Entity Framework stuff
 builder.Services.AddDbContext<WorldCitiesContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<WorldCitiesUser, IdentityRole>()
+builder.Services.AddIdentity<WorldCitiesUser, IdentityRole>(options =>
+    {
+        options.Password.RequiredLength = 8;
+    })
     .AddEntityFrameworkStores<WorldCitiesContext>();
+
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options => {
+    options.TokenValidationParameters = new() {
+        RequireExpirationTime = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecurityKey"]!))
+    };
+});
+builder.Services.AddScoped<JwtHandler>();
 
 WebApplication app = builder.Build();
 
@@ -34,6 +89,7 @@ if (app.Environment.IsDevelopment())
 app.UseCors("AllowAnything");
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
